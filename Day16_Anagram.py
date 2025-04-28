b6826c421214905b6e9e6c7130cbf54327a0a3b5
Kartikey Gupta <135418870+kartikey-here@users.noreply.github.com>
2025-04-28 07:33:24 +0530

    Create Day16_Anagram.py

---
+++ Day16_Anagram.py
@@ -0,0 +1,30 @@
+#Given two strings s1 and s2 consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, "act" and "tac" are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
+#Note: You can assume both the strings s1 & s2 are non-empty.
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def areAnagrams(self, s1, s2):
+        s1=list(s1)
+        s2=list(s2)
+        s1.sort()
+        s2.sort()
+        return s1==(s2)
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        a = input().strip()
+        b = input().strip()
+        if (Solution().areAnagrams(a, b)):
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
