a695565e5a6756674c5f2e4c2787e322f0d03c47
Kartikey Gupta <135418870+kartikey-here@users.noreply.github.com>
2025-05-09 20:04:39 +0530

    Create Day28_Number_Of_Occurences.py

---
+++ Day28_Number_Of_Occurences.py
@@ -0,0 +1,29 @@
+#Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+
+class Solution:
+    def countFreq(self, arr, target):
+        return arr.count(target)
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import bisect
+#Main
+if __name__ == '__main__':
+    t = int(input())
+    while t:
+        t -= 1
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ans = ob.countFreq(A, D)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
