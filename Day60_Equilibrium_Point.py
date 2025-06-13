87ad378c5ee94f0626ed3d4c46ba17ca6d6124fa
Kartikey Gupta <135418870+kartikey-here@users.noreply.github.com>
2025-06-13 22:53:14 +0530

    Create Day60_Equilibrium_Point.py

---
+++ Day60_Equilibrium_Point.py
@@ -0,0 +1,20 @@
+#Given an array of integers arr[], the task is to find the first equilibrium point in the array.
+
+#The equilibrium point in an array is an index (0-based indexing) such that the sum of all elements before that index is the same as the sum of elements after it. Return -1 if no such point exists. 
+
+
+# User function Template for python3
+
+class Solution:
+    #Function to find equilibrium point in the array.
+    def findEquilibrium(self, arr):
+        left_sum = arr[0]
+        right_sum = sum(arr)
+        for i in range(1, len(arr)):
+            left_sum += arr[i - 1]
+            right_sum -= arr[i]
+            if left_sum == right_sum:
+                return i
+        return -1
+        # code here
+
